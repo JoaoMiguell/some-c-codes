4298b36b3d5ed9a377ad1cb98e933691d64f89a4
Joao Miguell <joaomiguel_sm@outlook.com>
2023-08-03 21:31:18 -0300

    debug settings, pushback LK

--- linkedList.c
+++ linkedList.c
@@ -6,7 +6,7 @@ typedef struct Person {
     struct Person* next;
 } Person;
 
-void gen(Person* person) {
+Person* gen(struct Person* person) {
     const char* names[] = {"Ana", "Pedro", "Mariana", "Carlos", "Joana", "Rafael", "Lucia", "Daniel", "Juliana", "Luiz"};
     for(unsigned int i=0;i <= (sizeof(names)/sizeof(names[0]))-1; i++) {
         Person* p = malloc(sizeof(Person));
@@ -14,12 +14,25 @@ void gen(Person* person) {
         person->next = p;
         person = p;
     }
+
+    return person;
+}
+
+Person* pushBackLK(struct Person* lastPerson, const char* name) {
+    Person* p = malloc(sizeof(Person));
+    p->name = name;
+    lastPerson->next = p;
+    lastPerson = p;
+    lastPerson->next = NULL;
+
+    return lastPerson;
 }
 
 int main() {
-    Person person;
-    person.name = "me";
-    gen(&person);
-    
+    Person* person = malloc(sizeof(Person));
+    person->name = "me";
+    Person* last = gen(person);
+    last = pushBackLK(last, "charizard");
+    printf(last->name);
     return 0;
 }
